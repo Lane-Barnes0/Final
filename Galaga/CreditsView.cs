9c03d72704c8518508e500a2d88db64f96f6ad25
Lane Barnes <lanetravisbarnes@gmail.com>
2023-04-25 17:39:48 -0600

    Credits

--- Galaga/CreditsView.cs
+++ Galaga/CreditsView.cs
@@ -14,7 +14,10 @@ namespace Galaga
     {
         private SpriteFont m_font;
         private Texture2D m_background;
-        private const string MESSAGE = "Made by Lane Barnes";
+        private const string MESSAGE = "Made by \n";
+        private const string MESSAGE1 = "Lane Barnes";
+        private const string MESSAGE2 = "James Seelos";
+        private const string MESSAGE3 = "Thomas Lau";
 
         public override void loadContent(ContentManager contentManager)
         {
@@ -38,7 +41,19 @@ namespace Galaga
             m_spriteBatch.Draw(m_background, new Rectangle(0, 0, m_graphics.PreferredBackBufferWidth, m_graphics.PreferredBackBufferHeight), Color.White);
             Vector2 stringSize = m_font.MeasureString(MESSAGE);
             m_spriteBatch.DrawString(m_font, MESSAGE,
-                new Vector2(m_graphics.PreferredBackBufferWidth / 2 - stringSize.X / 2, m_graphics.PreferredBackBufferHeight / 2 - stringSize.Y), Color.Yellow);
+                new Vector2(m_graphics.PreferredBackBufferWidth / 2 - stringSize.X / 2, m_graphics.PreferredBackBufferHeight / 2 - stringSize.Y - 200), Color.Yellow);
+
+            stringSize = m_font.MeasureString(MESSAGE1);
+            m_spriteBatch.DrawString(m_font, MESSAGE1,
+                new Vector2(m_graphics.PreferredBackBufferWidth / 2 - stringSize.X / 2, m_graphics.PreferredBackBufferHeight / 2 - stringSize.Y - 150), Color.Yellow);
+
+            stringSize = m_font.MeasureString(MESSAGE2);
+            m_spriteBatch.DrawString(m_font, MESSAGE2,
+                new Vector2(m_graphics.PreferredBackBufferWidth / 2 - stringSize.X / 2, m_graphics.PreferredBackBufferHeight / 2 - stringSize.Y - 100), Color.Yellow);
+
+            stringSize = m_font.MeasureString(MESSAGE3);
+            m_spriteBatch.DrawString(m_font, MESSAGE3,
+                new Vector2(m_graphics.PreferredBackBufferWidth / 2 - stringSize.X / 2, m_graphics.PreferredBackBufferHeight / 2 - stringSize.Y - 50), Color.Yellow);
 
             m_spriteBatch.End();
         }
